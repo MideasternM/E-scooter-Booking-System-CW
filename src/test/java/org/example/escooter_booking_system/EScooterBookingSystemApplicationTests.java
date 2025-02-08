6c7469ae6cb82e88eadacd0deb45a898e94bca98
MideasternM <2608943659@qq.com>
2025-02-08 19:11:14 +0800

    first

---
+++ src/test/java/org/example/escooter_booking_system/EScooterBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.escooter_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EScooterBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
